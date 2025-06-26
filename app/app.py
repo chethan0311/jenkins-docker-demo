9e516102ceda458a04dc8e1a42814eeae44e94d5
Chethan Kumar <Chethanvsp@gmail.com>
2025-06-26 19:11:03 +0530

    files moved to app

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(_name_)
 
 @app.route('/')
 def hello():
-return "Hello from jenkins+Docker+GitHub!"
+    return "Hello from jenkins+Docker+GitHub!"
 
-if _name_ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
