894f6548a03487fcea7b5d46277e09f07340cc9e
Chethan Kumar <Chethanvsp@gmail.com>
2025-06-26 17:12:10 +0530

    Intial commit - Jenkins Docker app

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(_name_)
+
+@app.route('/')
+def hello();
+return "Hello from jenkins+Docker+GitHub!"
+
+if _name_ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
